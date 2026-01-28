2142e9c5670bd24593f5148b6c6327b2e520efb3
Deniz Telci <deniza.telci@gmail.com>
2026-01-27 20:05:15 -0600

    fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_01_28_000517) do
+ActiveRecord::Schema[8.0].define(version: 2026_01_28_020218) do
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "author"
-    t.decimal "price"
-    t.datetime "published_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
